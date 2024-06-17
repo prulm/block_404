8d4e4e8cd0f7585e1f0714061f2f16482b4c40a0
Yared <jaredyared83@gmail.com>
2024-06-17 21:33:13 +0330

    report model created

---
+++ lib/app/models/report.dart
@@ -0,0 +1,42 @@
+class Report {
+  final int id;
+  final String name;
+  final String description;
+  final List<String>? attachments;
+  final List<String>? pictures;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+  Report({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.attachments,
+    required this.pictures,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+      'attachments': attachments,
+      'pictures': pictures,
+      'created_at': createdAt,
+      'updated_at': updatedAt,
+    };
+  }
+
+  factory Report.fromMap(Map<String, dynamic> map) {
+    return Report(
+      id: map['id'],
+      name: map['name'],
+      description: map['description'],
+      attachments: map['attachments'],
+      pictures: map['pictures'],
+      createdAt: map['created_at'],
+      updatedAt: map['updated_at'],
+    );
+  }
+}
