a9a2aad69ac3f33d546607e49cd7c9129203745d
Yared <jaredyared83@gmail.com>
2024-06-17 22:18:32 +0330

    committee model created

--- lib/app/models/committee.dart
+++ lib/app/models/committee.dart
@@ -1,21 +1,25 @@
+import 'package:block_404/app/models/building.dart';
 import 'package:block_404/app/models/rule.dart';
 import 'package:block_404/app/models/user.dart';
 
 class Committee {
   final int id;
   final String description;
+  final String? attachments;
   final List<User> members;
   final User leader;
   final Building building;
   final List<Rule> committeeRules;
   final DateTime createdAt;
   final DateTime updatedAt;
-  Report({
+  Committee({
     required this.id,
-    required this.name,
     required this.description,
     required this.attachments,
-    required this.pictures,
+    required this.members,
+    required this.leader,
+    required this.building,
+    required this.committeeRules,
     required this.createdAt,
     required this.updatedAt,
   });
@@ -23,22 +27,30 @@ class Committee {
   Map<String, dynamic> toMap() {
     return {
       'id': id,
-      'name': name,
       'description': description,
       'attachments': attachments,
-      'pictures': pictures,
+      'members': members,
+      'leader': leader,
+      'building': building,
+      'committee_rules': committeeRules,
       'created_at': createdAt,
       'updated_at': updatedAt,
     };
   }
 
-  factory Report.fromMap(Map<String, dynamic> map) {
-    return Report(
+  factory Committee.fromMap(Map<String, dynamic> map) {
+    List<User> tempMembers = [];
+    List<Rule> tempRules = [];
+    map['members'].forEach((user) => tempMembers.add(User.fromMap(user)));
+    map['committee_rules'].forEach((rule) => tempRules.add(Rule.fromMap(rule)));
+    return Committee(
       id: map['id'],
-      name: map['name'],
       description: map['description'],
       attachments: map['attachments'],
-      pictures: map['pictures'],
+      members: tempMembers,
+      leader: User.fromMap(map['leader']),
+      building: Building.fromMap(map['building']),
+      committeRules: tempRules,
       createdAt: map['created_at'],
       updatedAt: map['updated_at'],
     );
