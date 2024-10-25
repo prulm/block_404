5abe6efa8099447e301d49562b5665e32107f895
Yared <jaredyared83@gmail.com>
2024-10-25 05:22:18 +0300

    matched model to api response

--- lib/app/models/rule.dart
+++ lib/app/models/rule.dart
@@ -1,14 +1,14 @@
 class Rule {
   final int id;
-  final String description;
-  final String? attachment;
+  final String rule;
+  final int order;
   final String? picture;
   final DateTime createdAt;
   final DateTime updatedAt;
   Rule({
     required this.id,
-    required this.description,
-    required this.attachment,
+    required this.rule,
+    required this.order,
     required this.picture,
     required this.createdAt,
     required this.updatedAt,
@@ -17,8 +17,8 @@ class Rule {
   Map<String, dynamic> toMap() {
     return {
       'id': id,
-      'description': description,
-      'attachment': attachment,
+      'rule': rule,
+      'order': order,
       'picture': picture,
       'created_at': createdAt,
       'updated_at': updatedAt,
@@ -28,8 +28,8 @@ class Rule {
   factory Rule.fromMap(Map<String, dynamic> map) {
     return Rule(
         id: map['id'],
-        description: map['description'],
-        attachment: map['attachmet'],
+        rule: map['rule'],
+        order: map['order'],
         picture: map['picture'],
         createdAt: DateTime.parse(map['created_at']),
         updatedAt: DateTime.parse(map['updated_at']));
