e4f063c1b4d25e9255ba624945d348bc7505a8b6
Yared <jaredyared83@gmail.com>
2024-10-25 05:26:57 +0300

    dummy user to be set up

--- lib/utils/dummy.dart
+++ lib/utils/dummy.dart
@@ -3,22 +3,14 @@ import 'package:block_404/utils/constants.dart';
 
 final List<List<dynamic>> names = [
   ['Yared', 'Fekade', '+251984872232', '', true, true],
-  ['Fikremariyam', 'Fekade', '+251984872232', profileUrl, true, false],
-  ['Bethel', 'Fekade', '+251984872232', profileUrl, false, true],
-  ['Seferu', 'Fekade', '+251984872232', profileUrl, false, false],
+  ['Shalom', 'Fekade', '+251943032826', profileUrl, true, false],
+  ['Girum', 'Fekade', '+251984872232', profileUrl, false, true],
+  ['Feven', 'Yared', '+251984872232', profileUrl, false, false],
 ];
 
-final List<User> dummyUsers = names
-    .asMap()
-    .entries
-    .map((entry) => User(
-        id: entry.key,
-        firstName: entry.value[0],
-        lastName: entry.value[1],
-        phone: entry.value[2],
-        profilePicture: entry.value[3],
-        isOwner: entry.value[4],
-        isResident: entry.value[5]))
-    .toList();
+final List<User> dummyUsers =
+    names.asMap().entries.map((entry) => User(
+      
+    )).toList();
 
 final User dummyCurrentUser = dummyUsers[0];
