cb0fb7522633031f89cd7936d8d2ea5e119c18eb
Yared <jaredyared83@gmail.com>
2024-10-28 03:40:13 +0300

    dummy profile created

--- lib/utils/dummy.dart
+++ lib/utils/dummy.dart
@@ -2,15 +2,60 @@ import 'package:block_404/app/models/user.dart';
 import 'package:block_404/utils/constants.dart';
 
 final List<List<dynamic>> names = [
-  ['Yared', 'Fekade', '+251984872232', '', true, true],
-  ['Shalom', 'Fekade', '+251943032826', profileUrl, true, false],
-  ['Girum', 'Fekade', '+251984872232', profileUrl, false, true],
-  ['Feven', 'Yared', '+251984872232', profileUrl, false, false],
+  [
+    1,
+    'Yared',
+    'Fekade',
+    '+251984872232',
+    'yaredfekade@gmail.com',
+    '',
+    true,
+    true
+  ],
+  [
+    1,
+    'Feven',
+    'Yared',
+    '+251984872232',
+    'fevenyared@gmail.com',
+    profileUrl,
+    true,
+    true
+  ],
+  [
+    1,
+    'Shalom',
+    'Fekede',
+    '+251984872232',
+    'shalomfekede@gmail.com',
+    profileUrl,
+    true,
+    true
+  ],
+  [
+    1,
+    'Girum',
+    'Fikadu',
+    '+251984872232',
+    'girumfikadu@gmail.com',
+    profileUrl,
+    true,
+    true
+  ],
 ];
 
-final List<User> dummyUsers =
-    names.asMap().entries.map((entry) => User(
-      
-    )).toList();
+final List<User> dummyUsers = names
+    .asMap()
+    .entries
+    .map((entry) => User(
+        id: entry.value[0],
+        firstName: entry.value[1],
+        lastName: entry.value[2],
+        phone: entry.value[3],
+        email: entry.value[4],
+        profilePicture: entry.value[5],
+        isSuperuser: entry.value[6],
+        isStaff: entry.value[7]))
+    .toList();
 
 final User dummyCurrentUser = dummyUsers[0];
